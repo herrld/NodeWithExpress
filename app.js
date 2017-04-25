9dfcadb8daf49f76807a876b016bdabae49844aa
herrld <lee.herring@enspiresoftware.com>
2017-04-24 21:24:48 -0400

    initial setup and installation of supporting libraries

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('source/views'));
+
+app.get('/', function (req, res) {
+    res.send("hello jill");
+});
+app.get('/books',function(req, res){
+   res.send("hello books"); 
+});
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
